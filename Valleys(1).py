d2faa7ab3569565e212344cc1347fa09660a5a15
savio <72309218+savio2000@users.noreply.github.com>
2022-12-04 07:25:40 +0530

    Create vally.py

---
+++ Valleys(1).py
@@ -0,0 +1,47 @@
+#what is valley?
+
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    level=0
+    val=0
+    p={"U":1,"D":-1}
+    for i in path:
+        level+=p[i]
+        if i=='U' and level==0:
+            val+=1
+    return val        
+            
+            
+            
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
